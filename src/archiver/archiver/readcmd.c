0a4393c8a92291d02304305373e30b1b676e8329
benjamin dischinger <benjamin.dischinger@sun.com>
2008-06-04 14:59:56 -0500

    6697120 RFE: Remove the -join command from the archiver.cmd ---- Review Text:    Made -join an invalid archive set option. Commit Text:
    Associated Bugtraq Numbers:
      6697120: RFE: Remove the -join command from the archiver.cmd
    
    SourceCset: c79df3647641

--- src/archiver/archiver/readcmd.c
+++ src/archiver/archiver/readcmd.c
@@ -31,7 +31,7 @@
  *    SAM-QFS_notice_end
  */
 
-#pragma ident "$Revision: 1.111 $"
+#pragma ident "$Revision: 1.112 $"
 
 static char *_SrcFile = __FILE__;   /* Using __FILE__ makes duplicate strings */
 
@@ -795,6 +795,7 @@ static DirProc_t table[] = {
 	{ "-simread",		paramsInvalid,		DP_other },
 	{ "-tstovfl",		paramsInvalid,		DP_other },
 #endif /* !defined(DEBUG) | defined(lint) */
+	{ "-join",			paramsInvalid,		DP_other },
 	{ "-fillvsns",		paramsSetFillvsns,  DP_other },
 	{ NULL,	paramsSetfield, DP_other }
 };
