84cdb75dacf1bcb0c1f248abd5f1698cffff1dfb
bradford blasing <bradford.blasing@sun.com>
2008-12-11 09:18:28 -0600

    6757712 after shared client host off/on, client failed to mount FS again ---- Review Text:      putLabel() was updating the generation number   in the label block (from the host table) before it writes it.  However,   the generation number in the internal copy of the label block in the   configuration table was not being kept up to date. Commit Text:
    Associated Bugtraq Numbers:
      6757712: after shared client host off/on, client failed to mount FS again
    
    SourceCset: b5cb998585fc

--- src/fs/sharefs/config.c
+++ src/fs/sharefs/config.c
@@ -31,7 +31,7 @@
  *    SAM-QFS_notice_end
  */
 
-#pragma ident "$Revision: 1.75 $"
+#pragma ident "$Revision: 1.76 $"
 
 static char *_SrcFile = __FILE__;   /* Using __FILE__ makes duplicate strings */
 
@@ -181,6 +181,7 @@ putLabel(
 	lb->version = SAM_LABEL_VERSION;
 	lb->serverord = cfp->ht->info.ht.server;
 	lb->gen = cfp->ht->info.ht.gen;
+	cfp->lb.info.lb.gen = lb->gen;	/* keep config label gen up to date */
 	strncpy(lb->server, cfp->serverName, sizeof (lb->server));
 	strncpy(lb->serveraddr, cfp->serverAddr, sizeof (lb->serveraddr));
 
