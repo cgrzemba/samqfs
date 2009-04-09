efeb21ddcb499b8cab1a3c24072cc335b3498787
kenichiro kagoshima <kenichiro.kagoshima@sun.com>
2009-04-09 07:10:38 -0500

    6826497 sam-stagerd_copy always issues direct io ---- Review Text:
      Don't set an errno to readErrno if stage was cancelled by stage -x.
    Commit Text:
    
    Associated Bugtraq Numbers:
      6826497: sam-stagerd_copy always issues direct io
    
    SourceCset: 7615216641b1

--- src/stager/copy/disk_cache.c
+++ src/stager/copy/disk_cache.c
@@ -31,7 +31,7 @@
  *    SAM-QFS_notice_end
  */
 
-#pragma ident "$Revision: 1.7 $"
+#pragma ident "$Revision: 1.8 $"
 
 static char *_SrcFile = __FILE__; /* Using __FILE__ makes duplicate strings */
 
@@ -251,11 +251,11 @@ DiskCacheWrite(
 				    file->id.ino, file->id.gen);
 				SET_FLAG(IoThread->io_flags, IO_cancel);
 
-				readErrno = errno;
-				if (readErrno == ECANCELED) {
+				if (errno == ECANCELED) {
 					cancel = B_TRUE;
 					SET_FLAG(file->flags, FI_CANCEL);
 				} else {
+					readErrno = errno;
 					SET_FLAG(file->flags, FI_WRITE_ERROR);
 				}
 			}
