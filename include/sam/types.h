99aab130c0f0f95ce679932f141966caa781176f
Carsten Grzemba <cgrzemba@opencsw.org>
2023-01-27 14:11:49 +0100

    update Copyright

--- include/sam/types.h
+++ include/sam/types.h
@@ -111,7 +111,7 @@ typedef unsigned long long uint64_t;
 #endif
 
 /* The following is for the lawyers */
-#define	COPYRIGHT "(C) 2010-2022, Use is subject to license terms."
+#define	COPYRIGHT "(C) 2010-2023, Use is subject to license terms."
 
 #define	RESELL_LIB "libresel.so"
 
