3ff7ef111789ae393b10e8924141262bade522ed
Carsten Grzemba <cgrzemba@opencsw.org>
2023-02-24 16:34:16 +0100

    bump mariadb version 10.8

--- mk/include/omnios.mk
+++ mk/include/omnios.mk
@@ -3,13 +3,15 @@ GCC=/opt/gcc-10/bin/gcc
 DB_INCLUDE=/opt/ooce/include
 DB_LIB= -L/opt/ooce/lib$(ISA) -R/opt/ooce/lib$(ISA) -ldb-5.3 
 
-MYSQL_VERSION=mariadb-10.4
+MYSQL_VERSION=mariadb-10.8
 MYSQL_INCLUDE=-I/opt/ooce/$(MYSQL_VERSION)/include/mysql
 MYSQL_LIB=-L/opt/ooce/$(MYSQL_VERSION)/lib$(ISA) -R/opt/ooce/$(MYSQL_VERSION)/lib$(ISA) -lmysqlclient
 
 # OS-3294 add inotify support
 OSDEPCFLAGS += -D_INOTIFY
 
-SSL_INCLUDES = -I/usr/ssl-1.0/include
-SSL_LIBDIR = -L/usr/ssl-1.0/lib -R/usr/ssl-1.0/lib
+# SSL_INCLUDES = -I/usr/ssl-1.0/include
+# SSL_LIBDIR = -L/usr/ssl-1.0/lib -R/usr/ssl-1.0/lib
 
+CTFCONVERT = /opt/onbld/bin/i386/ctfconvert
+CTFMERGE = /opt/onbld/bin/i386/ctfmerge
