213c10dbbbdabad79cb9763192f175af680538b6
Carsten Grzemba <cgrzemba@opencsw.org>
2025-11-27 11:24:56 +0100

    use mariadb 10.8, revert commit 3a50b77

--- mk/include/omnios.mk
+++ mk/include/omnios.mk
@@ -3,7 +3,7 @@ GCC=/opt/gcc-14/bin/gcc
 DB_INCLUDE=/opt/ooce/include
 DB_LIB= -L/opt/ooce/lib$(ISA) -R/opt/ooce/lib$(ISA) -ldb-5.3 
 
-MYSQL_VERSION=mariadb-10.11
+MYSQL_VERSION=mariadb-10.8
 MYSQL_INCLUDE=-I/opt/ooce/$(MYSQL_VERSION)/include/mysql
 MYSQL_LIB=-L/opt/ooce/$(MYSQL_VERSION)/lib$(ISA) -R/opt/ooce/$(MYSQL_VERSION)/lib$(ISA) -lmysqlclient
 
